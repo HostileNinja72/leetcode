b9b34ecf21123c9f4777ebb14124c073f6d21728
Soufiane47 <soufianeastronomie@gmail.com>
2024-08-20 04:51:03 +0100

    First Challenge

---
+++ minSpeedOnTime.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <cmath>
+
+// leetcode 1870 Medium challenge
+// https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool canReachInTime(const vector<int> &dist, double hour, int speed)
+    {
+        double time = 0;
+        for (size_t i = 0; i < dist.size() - 1; ++i)
+            time += ceil(static_cast<double>(dist[i]) / speed);
+
+        time += static_cast<double>(dist.back()) / speed;
+        return time <= hour;
+    }
+
+    int minSpeedOnTime(vector<int> &dist, double hour)
+    {
+        int low = 1, high = 1e7;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (canReachInTime(dist, hour, mid))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return canReachInTime(dist, hour, low) ? low : -1;
+    }
+};
